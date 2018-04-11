4d46c0ab1dddb5e6d4510af4f0b813f695756dcd
S00112276 <S00112276@mail.itsligo.ie>
2018-04-11 20:24:03 +0100

    Repository pattern created / started repository

--- AttendanceAPI/DAL/IAttendanceTracker.cs
+++ AttendanceAPI/DAL/IAttendanceTracker.cs
@@ -9,10 +9,46 @@ namespace AttendanceAPI.DAL
 {
     public interface IAttendanceTracker
     {
-        // Get users, modules and deliveries
+        // Enrollment
+        Enrollment Enroll(Enrollment e);
+        Enrollment Disenroll(int id);
+
+        // Students
         Task<IList<Student>> GetStudents();
+        Task<Student> AddStudent(Student s);
+        Task<Student> UpdateStudent(Student s);
+        Task<Student> DeleteStudent(int id);
+        Student GetStudent(int id);
+
+        // Lecturers
         Task<IList<Lecturer>> GetLecturers();
+        Task<Lecturer> AddLecturer(Lecturer l);
+        Task<Lecturer> UpdateLecturer(Lecturer l);
+        Task<Lecturer> Deletelecturer(int id);
+        Lecturer GetLecturer(int id);
+
+        // Module
         Task<IList<Module>> GetModules();
+        Task<Module> AddModule(Module m);
+        Task<Module> UpdateModule(Module m);
+        Task<Module> DeleteModule(int id);
+        Module GetModule(int id);
+
+        // Deliveries
         Task<IList<Delivery>> GetDeliveries();
+        Task<Delivery> AddDelivery(Delivery d);
+        Task<Delivery> UpdateDelivery(Delivery d);
+        Task<Delivery> DeleteDelivery(int id);
+        Delivery GetDelivery(int id);
+
+        // Attendance
+        Task<Attendance> AddAttendance(Attendance a);
+        Task<Attendance> UpdateAttendance(Attendance a);
+        Attendance GetAttendance(int id);
+
+        // StudentAttendance
+        Task<StudentAttendance> AddStudentAttendance(StudentAttendance s);
+        Task<StudentAttendance> UpdateStudentAttendance(StudentAttendance s);
+        StudentAttendance GetStudentAttendance(int id);
     }
 }
