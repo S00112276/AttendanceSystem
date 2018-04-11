08a11b9a96e14d3bfc14b2a2b435492c1ae78ef0
Sophia Price <sophiaprice10@hotmail.com>
2018-04-11 23:02:38 +0100

    StudentsController Added
    Some API's added to StudentsController

---
+++ AttendanceAPI/Controllers/StudentsController.cs
@@ -0,0 +1,56 @@
+﻿using AttendanceAPI.DAL;
+using AttendanceAPI.Models;
+using AttendanceAPI.Models.Banner;
+using AttendanceAPI.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AttendanceAPI.Controllers
+{
+    //[Authorize(Roles = "Lecturer")]
+    //[RoutePrefix("api/attendance")]
+    public class StudentsController : ApiController
+    {
+        private AttendanceDB db = new AttendanceDB();
+
+        private AttendanceTrackerRepo context;
+
+        public StudentsController()
+        {
+            context = new AttendanceTrackerRepo(new AttendanceDB());
+        }
+
+        // For Injection
+        public StudentsController(AttendanceTrackerRepo ctx)
+        {
+            context = ctx;
+        }
+        
+        [HttpGet]
+        [Route("Students")]
+        public async Task<IList<Student>> GetStudents()
+        {
+            return await (context as AttendanceTrackerRepo).GetStudents();
+        }
+
+        [HttpGet]
+        [Route("Student/{id:int}")]
+        public async Task<IList<Student>> GetStudent(int id)
+        {
+            Student student = (context as AttendanceTrackerRepo).GetStudent(id);
+
+            if(student != null)
+            {
+                StudentDTO studentDTO;
+                //var enrollments = (context as AttendanceTrackerRepo).
+            }
+
+            return null;
+        }
+    }
+}
---
+++ AttendanceAPI/Models/DTO/StudentDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AttendanceAPI.Models.DTO
+{
+    public class StudentDTO
+    {
+        public int id { get; }
+        public string RegistrationID { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public virtual ICollection<Enrollment> EnrolledFor { get; set; }
+    }
+}
